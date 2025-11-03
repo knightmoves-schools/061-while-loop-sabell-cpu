c673b09a4a65f4e5c996d117420675f4a433e0ae
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-11-03 19:14:09 +0000

    loop de doop

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var todos = ['first todo', 'second todo', 'third todo'];
 
+function markAsDone(todos){
+
+    for(let i = 0; i < todos.length; i++){
+        todos[i] = 'done - ' + todos[i];
+    }
+    return todos;
+}
+
+document.getElementById('result').innerHTML = todos;
